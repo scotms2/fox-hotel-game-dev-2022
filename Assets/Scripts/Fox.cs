d38eb9674d6e066458434e77150dcbbf06510b86
mitch-scott <mitchellscott11@hotmail.com>
2022-03-16 16:11:49 +1300

    got an object spawning and moving towards a point

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    private Spawner foxSpawner;
+
+    public float runSpeed;
+
+    public GameObject runPoint;
+
+    private double MinDist = 0.5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        runPoint = GameObject.FindWithTag("Finish");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, runPoint.transform.position) >= MinDist)
+        {
+            transform.position += transform.right * runSpeed * Time.deltaTime;
+        }
+        
+    }
+
+    public void SetSpawner(Spawner spawner)
+    {
+        foxSpawner = spawner;
+    }
+}
