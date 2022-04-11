d7ed5feeac3def1975b1bd69c66bb78f6cffbc46
mitch-scott <mitchellscott11@hotmail.com>
2022-04-11 16:43:31 +1200

    added a limit to how far the camera can zoom in and out

--- Assets/Scripts/Camera/CameraScript.cs
+++ Assets/Scripts/Camera/CameraScript.cs
@@ -7,12 +7,25 @@ public class CameraScript : MonoBehaviour
     public float dragSpeed = 2;
     private Vector3 dragOrigin;
 
+    public Vector3 camOutPos;
+
+    public Vector3 camInPos;
+
     // Update is called once per frame
     void Update()
     {
         float ScrollWheel = Input.GetAxis("Mouse ScrollWheel");
-        Vector3 mousePos = Input.mousePosition;
 
         Camera.main.transform.position += Camera.main.transform.forward * ScrollWheel;
+
+        if(this.transform.position.z < -18)
+        {
+            this.transform.position = camOutPos;
+        }
+
+        if(this.transform.position.z > -6.5)
+        {
+            this.transform.position = camInPos;
+        }
     }
 }
