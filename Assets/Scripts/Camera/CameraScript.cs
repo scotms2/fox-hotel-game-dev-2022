fa7497abbe542430b8c6043139b1aa3db4276c91
mitch-scott <mitchellscott11@hotmail.com>
2022-04-14 17:08:01 +1200

    changed camera zoom and added camera drag movement

--- Assets/Scripts/Camera/CameraScript.cs
+++ Assets/Scripts/Camera/CameraScript.cs
@@ -4,28 +4,52 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    public float dragSpeed = 2;
+    public float dragSpeed;
     private Vector3 dragOrigin;
 
     public Vector3 camOutPos;
 
     public Vector3 camInPos;
 
+    private Vector3 oldPos;
+
     // Update is called once per frame
     void Update()
     {
         float ScrollWheel = Input.GetAxis("Mouse ScrollWheel");
-
-        Camera.main.transform.position += Camera.main.transform.forward * ScrollWheel;
-
-        if(this.transform.position.z < -18)
+        if (ScrollWheel < 0)
         {
-            this.transform.position = camOutPos;
+            //this.transform.position = camOutPos;
+            Camera.main.orthographic = false;
+        }
+        else if (ScrollWheel > 0)
+        {
+            //this.transform.position = camInPos;
+            Camera.main.orthographic = true;
         }
 
-        if(this.transform.position.z > -6.5)
-        {
-            this.transform.position = camInPos;
-        }
+        // Camera.main.transform.position += Camera.main.transform.forward * ScrollWheel;
+
+        // if(this.transform.position.z < camOutPos.z)
+        // {
+        //     this.transform.position = camOutPos;
+        // }
+
+        // if(this.transform.position.z > -6.5)
+        // {
+        //     this.transform.position = camInPos;
+        // }
+
+        if(Input.GetMouseButtonDown(0))
+         {
+            oldPos = transform.position;
+            dragOrigin = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+         }
+ 
+         if(Input.GetMouseButton(0))
+         {
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition) - dragOrigin;
+            transform.position = oldPos + -pos * dragSpeed;
+         }
     }
 }
