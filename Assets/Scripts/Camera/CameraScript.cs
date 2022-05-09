95abead8f075b3ccec5a3197b2eae45c9cae2539
mitch-scott <mitchellscott11@hotmail.com>
2022-05-09 16:33:14 +1200

    adding camera progress, have added input for touchscreen

--- Assets/Scripts/Camera/CameraScript.cs
+++ Assets/Scripts/Camera/CameraScript.cs
@@ -1,55 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CameraScript : MonoBehaviour
 {
     public float dragSpeed;
-    private Vector3 dragOrigin;
 
     public Vector3 camOutPos;
 
     public Vector3 camInPos;
 
-    private Vector3 oldPos;
+    private Vector3 touchStart;
+
+    private bool ZoomOut;
+
+    public TextMeshProUGUI txt;
 
     // Update is called once per frame
     void Update()
     {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitInfo;
+
         float ScrollWheel = Input.GetAxis("Mouse ScrollWheel");
+
         if (ScrollWheel < 0)
         {
-            //this.transform.position = camOutPos;
-            Camera.main.orthographic = false;
+            Camera.main.orthographic = true;
+            ZoomOut = true;
         }
         else if (ScrollWheel > 0)
         {
-            //this.transform.position = camInPos;
-            Camera.main.orthographic = true;
+            Camera.main.orthographic = false;
+            ZoomOut = false;
         }
 
-        // Camera.main.transform.position += Camera.main.transform.forward * ScrollWheel;
+        if(Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
 
-        // if(this.transform.position.z < camOutPos.z)
-        // {
-        //     this.transform.position = camOutPos;
-        // }
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
 
-        // if(this.transform.position.z > -6.5)
-        // {
-        //     this.transform.position = camInPos;
-        // }
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
 
-        if(Input.GetMouseButtonDown(0))
-         {
-            oldPos = transform.position;
-            dragOrigin = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-         }
- 
-         if(Input.GetMouseButton(0))
-         {
-            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition) - dragOrigin;
-            transform.position = oldPos + -pos * dragSpeed;
-         }
+            float difference = currentMagnitude - prevMagnitude;
+
+            txt.text = "Difference Value: " + difference.ToString();
+
+            if (difference < 0)
+            {
+                Camera.main.orthographic = true;
+                ZoomOut = true;
+            }
+            else if (difference > 0)
+            {
+                Camera.main.orthographic = false;
+                ZoomOut = false;
+            }
+        }
+
+        if(Camera.main.orthographic)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+    
+            if(Input.GetMouseButton(0))
+            {
+                Vector3 pos = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Camera.main.transform.position += pos;
+            }
+        }
+
+        if(Physics.Raycast (ray, out hitInfo))
+        {
+            Debug.Log(hitInfo.transform.tag);
+            Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
+        }
+        else {
+            Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.green);
+        }
     }
 }
